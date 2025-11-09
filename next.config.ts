ba3253a2dc6be95f8026d87fc6a13cf6bf37d618
ArazMaher <Araz.maher.88@gmail.com>
2025-11-09 11:58:34 +0330

    Fixed Images URL

--- next.config.ts
+++ next.config.ts
@@ -1,4 +1,34 @@
+// import type { NextConfig } from "next";
+
+// const nextConfig: NextConfig = {
+// 	reactStrictMode: true,
+// 	typescript: {
+// 		ignoreBuildErrors: true,
+// 	},
+// 	eslint: {
+// 		ignoreDuringBuilds: true,
+// 	},
+// 	experimental: {
+// 		optimizeCss: true,
+// 		reactCompiler: true,
+// 	},
+// 	images: {
+// 		remotePatterns: [
+// 			{
+// 				hostname: "raw.githubusercontent.com",
+// 			},
+// 			{
+// 				hostname: "images.unsplash.com",
+// 			},
+// 		],
+// 	},
+// };
+// export default nextConfig;
+
+
 import type { NextConfig } from "next";
+
+const repoName = "ai-lnading-page"
 
 const nextConfig: NextConfig = {
 	reactStrictMode: true,
@@ -22,5 +52,12 @@ const nextConfig: NextConfig = {
 			},
 		],
 	},
+	// 👇 این دو تا خط جادو هستن برای درست شدن مسیرها در GitHub Pages
+	basePath: `/${repoName}`,
+	assetPrefix: `/${repoName}/`,
+	env: {
+		NEXT_PUBLIC_BASE_PATH: `/${repoName}`,
+	},
 };
+
 export default nextConfig;
